212f22ecf9b519bd5086f3e7968a708a8f9106fe
Wren <155106259+DisketteKitchen@users.noreply.github.com>
2025-04-16 00:01:03 -0600

    Update index.vue with DK homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <main class="flex flex-col justify-center">
-    <HomeTwitter />
-    <HomeHero />
-    <HomeFeatures />
-    <HomeLogos />
-    <HomeCta />
-  </main>
+  <Landing />
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Calistoga&family=Imbue:opsz,wght@10..100,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+
+body {
+  margin: 0;
+}
+html { scroll-behavior: smooth; }
+
+.project-container {
+  width: 100%;
+  height: 865px;
+  background-color: #717171;
+  display: inline-flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
+<script setup lang="ts">
+</script>
